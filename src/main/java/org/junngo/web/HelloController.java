baa0947beefe595d8228177d2a7716a74548cda5
Myeongjun Ko <mjunko@dozn.co.kr>
2021-03-04 13:39:39 +0900

    added port

--- src/main/java/org/junngo/web/HelloController.java
+++ src/main/java/org/junngo/web/HelloController.java
@@ -1,12 +1,13 @@
 package org.junngo.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/callback")
+    @RequestMapping("/hello")
     public String hello() {
         return "hello";
     }
